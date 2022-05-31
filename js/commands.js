6c748b7c2fcc9d8f9deaae4be06a6276725005b9
Alejandro Harris <aharris@uninorte.edu.co>
2022-05-31 14:14:59 +0200

    Add files via upload

---
+++ js/commands.js
@@ -0,0 +1,124 @@
+var password = "guarapo";
+var linkedin = "https://www.linkedin.com/in/alejandroharris/";
+var github = "https://github.com/AlejoHarris";
+var email = "mailto:alejoharris@me.com";
+var nfts = "https://hic.af/alejoharris";
+
+whois = [
+  "<br>",
+  "Hey, I'm Alejandro!👋",
+  "Now that you're here, you can call me Alejo.",
+  "I'm a software developer and sometimes I make art.",
+  "<br>"
+];
+
+skills = [
+  "<br>",
+  "git                    ████ ",
+  "javascript/typescript  █████",
+  "python                 ████ ",
+  "java                   ████ ",
+  "c++                    ████ ",
+  "c#                     ███  ",
+  "dynamodb               █████",
+  "mongodb                █████",
+  "sql                    ████ ",
+  "jenkins                ████ ",
+  "docker                 ████ ",
+  "kubernetes             ███  ",
+  "amazon web services    ████ ",
+  "google cloud           ████ ",
+  "<br>",
+  "yeah, that's me with a hat",
+  "<br>"
+]
+
+profile = [
+  "<br>",
+  "Electronics Engineer and Master",
+  "in Visual Arts and Multimedia",
+  "with demostrated expertise in",
+  "planning and project development",
+  "abilites. In-depth problem solving",
+  "analysis and communication skills",
+  "Able to deliver creative and",
+  "innovative tasks. Proactive in",
+  "providing resolution to problems.",
+  "5 years of experience using",
+  "different technologies for web,",
+  "software and hardware development.",
+  "<br>",
+  "For more details, visit my LinkedIn",
+  "<br>",
+];
+
+social = [
+  "<br>",
+  "You can find me on:",
+  "<br>",
+  'linkedin      <a href="' +
+    linkedin +
+    '" target="_blank">linkedin/alejoharris' +
+    "</a>",
+  'github        <a href="' +
+    github +
+    '" target="_blank">github/alejoharris' +
+    "</a>",
+  'nft?          <a href="' + nfts + '" target="_blank">not anymore' + "</a>",
+  "<br>",
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Are you sure you\'re an admin?',
+  "<br>",
+];
+
+projects = [
+  "<br>",
+  "Most projects are offline, on GitHub, or confidential.",
+  "But you can see some of them in action:",
+  "<br>",
+  '<a href="https://vitamin-arte.com/portfolio/frequency-powehi-volumens-festival-instalacion/" target="_blank">powehi</a>                    | with Vitamin' ,  
+  '<a href="http://101.vitamin-arte.com/" target="_blank">101</a>                       | for Vitamin',
+  '<a href="https://vitamin-arte.com/portfolio/noken-experiencia-interactiva/" target="_blank">noken</a>                     | for Vitamin',
+  '<a href="https://vitamin-arte.com/portfolio/coolway-cooldance-instalacion-interactiva/">cooldance</a>                 | for Vitamin',
+  '<a href="https://vitamin-arte.com/portfolio/mahoucleta-instalacion-cinetica/">mahoucleta</a>                | for Vitamin',
+  '<a href="https://vitamin-arte.com/portfolio/astral-bodies-feria-habitat/">astral bodies</a>             | for Vitamin',
+  '<a href="https://vitamin-arte.com/portfolio/voltereta-instalacion-luminica-inmersiva-2/">voltereta manhattan</a>       | for Vitamin',
+  '<a href="https://contactless.felipepantone.com/" target="_blank">contactless</a>               | for Felipe Pantone',
+  '<a href="https://isp01.configurableart.com/" target="_blank">integration system p#01</a>   | for Felipe Pantone',
+  "<br>",
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Alejandro?',
+  '<span class="command">profile</span>        Professional Profile',
+  '<span class="command">skills</span>         Technical Skills',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  '<span class="command">email</span>          Let\'s talk!',
+  '<span class="command">help</span>           Ehm...',
+  "<br>",
+];
+
+banner = [
+  `<span class="index">Welcome to your ${navigator.platform} OS terminal</span>`,
+  "<br>",
+  "   █████╗ ██╗     ███████╗     ██╗ ██████╗     ██╗  ██╗ █████╗ ██████╗ ██████╗ ██╗███████╗",
+  "  ██╔══██╗██║     ██╔════╝     ██║██╔═══██╗    ██║  ██║██╔══██╗██╔══██╗██╔══██╗██║██╔════╝",
+  "  ███████║██║     █████╗       ██║██║   ██║    ███████║███████║██████╔╝██████╔╝██║███████╗",
+  "  ██╔══██║██║     ██╔══╝  ██   ██║██║   ██║    ██╔══██║██╔══██║██╔══██╗██╔══██╗██║╚════██║",
+  "  ██║  ██║███████╗███████╗╚█████╔╝╚██████╔╝    ██║  ██║██║  ██║██║  ██║██║  ██║██║███████║",
+  "  ╚═╝  ╚═╝╚══════╝╚══════╝ ╚════╝  ╚═════╝     ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝╚══════╝",
+  "                      ╔╗ ╔═╗╔═╗╦╔═╔═╗╔╗╔╔╦╗  ╔╦╗╔═╗╦  ╦╔═╗╦  ╔═╗╔═╗╔═╗╦═╗                 ",
+  "                      ╠╩╗╠═╣║  ╠╩╗║╣ ║║║ ║║   ║║║╣ ╚╗╔╝║╣ ║  ║ ║╠═╝║╣ ╠╦╝                 ",
+  "                      ╚═╝╩ ╩╚═╝╩ ╩╚═╝╝╚╝═╩╝  ═╩╝╚═╝ ╚╝ ╚═╝╩═╝╚═╝╩  ╚═╝╩╚═                 ",
+  "<br>",
+  '<span class="color2">Type</span> <span class="command">\'help\'</span><span class="color2"> to display your list of available commands.</span>',
+];
